8c4178f3e881be435dc0314e1cf009ef64ef908b
Sunny Chan <sunnyucb@gmail.com>
2017-07-11 14:38:55 -0700

    Added initial xcode stuffs.

---
+++ SwiftLeetCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLeetCode
+//
+//  Created by Sunny Chan on 7/11/17.
+//  Copyright © 2017 sunnystudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
